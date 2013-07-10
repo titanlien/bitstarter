04a3f736a930d9c6466de5afb042f2e9150eff17
Titan <titan.lien@gmail.com>
2013-07-10 02:24:50 +0000

    read index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
+var indexBuf = fs.readFileSync('index.html', String);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexBuf);
 });
 
 var port = process.env.PORT || 5000;
